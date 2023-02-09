851ee3e9ba1c4cc13252f15741cfb847256044cd
OSRoot <osamaessayed.el.4468@azhar.edu.eg>
2023-02-09 09:04:58 -0800

    final Attempt

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=arn:aws:s3:::elasticbeanstalk-us-east-1-761924512277
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-# export URL=http://localhost:8100
+export URL=http://localhost:8100
