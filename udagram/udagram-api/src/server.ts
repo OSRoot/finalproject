df2372a30a45d75b7d5df9d5a57be27c0c4ecf11
OSRoot <osamaessayed.el.4468@azhar.edu.eg>
2023-02-09 00:07:24 -0800

    Final Attempt

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -49,7 +49,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/ Circleci updating");
+    res.send("/api/v0/ Final attempt from CircleCi");
   });
 
   // Start the Server
